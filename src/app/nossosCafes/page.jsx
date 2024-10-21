8d0767b202a843a3e6f85ad6c8c98f42e94c60de
Marcello <mgbirkan@icloud.com>
2024-10-20 22:24:48 -0300

    Refact: Criado componentes internos para facilitar leitura do código.

--- src/app/nossosCafes/page.jsx
+++ src/app/nossosCafes/page.jsx
@@ -7,6 +7,15 @@ const stats = [
   { id: 4, name: 'Cafés servidos desde a inauguração', value: '150,000+' },
 ]
 
+function InfoPanel(props) {
+  return <div className="flex flex-col bg-white/5 p-8">
+    <dt
+      className="text-sm font-semibold leading-6 text-gray-300">{props.stat.name}</dt>
+    <dd
+      className="order-first text-3xl font-semibold tracking-tight text-white">{props.stat.value}</dd>
+  </div>;
+}
+
 export default function Example() {
   return (
     <div className="bg-amber-900 py-24 sm:py-32 h-screen">
@@ -23,10 +32,7 @@ export default function Example() {
           </div>
           <dl className="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
             {stats.map((stat) => (
-              <div key={stat.id} className="flex flex-col bg-white/5 p-8">
-                <dt className="text-sm font-semibold leading-6 text-gray-300">{stat.name}</dt>
-                <dd className="order-first text-3xl font-semibold tracking-tight text-white">{stat.value}</dd>
-              </div>
+              <InfoPanel key={stat.id} stat={stat}/>
             ))}
           </dl>
         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,36 @@
 import Link from 'next/link';
-import {LinkPersonalizado} from "@/app/components/LinkPersonalizado";
+
+function CtaButton() {
+  return <div className="mt-10 flex items-center gap-x-6">
+    <Link
+      href="#"
+      className="rounded-md bg-amber-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600"
+    >
+      Peça agora
+    </Link>
+    <LinkPersonalizado href={"/nossosCafes"}>Nossos Cafés</LinkPersonalizado>
+  </div>;
+}
+
+const LinkPersonalizado = ({children, href}) => {
+  return (
+    <Link href={href}
+          className="whitespace-nowrap font-semibold text-amber-600">
+      {children} <span>&rarr;</span>
+    </Link>
+  )
+}
+
+function VerticalImage() {
+  return <div
+    className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
+    <img
+      alt=""
+      src="https://cdn.midjourney.com/01522bc3-f91b-4883-8644-6ca3fa2128f6/0_0.png"
+      className="aspect-[3/2] w-full bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
+    />
+  </div>;
+}
 
 export default function Page() {
   return (
@@ -29,25 +60,10 @@ export default function Page() {
               Saboreie nossos cafés especiais e sinta o aroma que encanta. Cada
               xícara é preparada com carinho e dedicação.
             </p>
-            <div className="mt-10 flex items-center gap-x-6">
-              <Link
-                href="#"
-                className="rounded-md bg-amber-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600"
-              >
-                Peça agora
-              </Link>
-              <LinkPersonalizado href={"/nossosCafes"}>Nossos Cafés</LinkPersonalizado>
-            </div>
+            <CtaButton/>
           </div>
         </div>
-        <div
-          className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
-          <img
-            alt=""
-            src="https://cdn.midjourney.com/01522bc3-f91b-4883-8644-6ca3fa2128f6/0_0.png"
-            className="aspect-[3/2] w-full bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
-          />
-        </div>
+        <VerticalImage/>
       </div>
     </div>
   )
