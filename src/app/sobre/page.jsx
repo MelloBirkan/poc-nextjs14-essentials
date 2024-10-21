b6437bc80991aa327461bdbbb9b834b48d1bef34
Marcello <mgbirkan@icloud.com>
2024-10-20 22:04:28 -0300

    Componente Breadcrumb criado

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -1,6 +1,11 @@
+import Breadcrumbs from "@/app/components/Breadcrumbs";
+
 export default function Sobre() {
   return (
     <div className="bg-white px-6 py-24 sm:py-32 lg:px-8 h-screen">
+      <Breadcrumbs paginas={[
+        { name: 'Sobre', href: '/sobre', current: true },
+      ]}/>
       <div className="mx-auto max-w-2xl text-center">
         <p className="text-base font-semibold leading-7 text-amber-600">Conheça nossa cafeteria</p>
         <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Sobre nós</h2>
